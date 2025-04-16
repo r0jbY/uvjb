3d7a89341047c5f43fdae89f264f4284f5effe2b
r0jbY <r.balint@student.fontys.nl>
2025-04-16 12:24:34 +0200

    User Service setup
    Set up the skeleton for the user service.

---
+++ Implementation/MicroserviceBackend/UserService/jest.config.js
@@ -0,0 +1,14 @@
+export const preset = "ts-jest";
+export const testEnvironment = "node";
+export const rootDir = "./";
+export const testMatch = ["**/?(*.)+(spec|test).[tj]s?(x)"];
+export const collectCoverage = true;
+export const collectCoverageFrom = [
+  "src/**/*.{ts,tsx}", // <-- include all TS/TSX files under /src
+  "!src/**/*.d.ts", // <-- ignore type declaration files
+  "!src/**/__tests__/**", // <-- ignore test folders if you have any inside src
+  "!src/**/index.ts", // <-- optional: ignore index.ts entry points
+  "!src/**/app.ts", // <-- optional: ignore index.ts entry points
+  "!src/config/**", // <-- optional: ignore config files
+];
+export const testPathIgnorePatterns = ['/dist'];
