828f4161de4d19083fd553bac161736c3e45bc3b
r0jbY <r.balint@student.fontys.nl>
2025-05-06 17:30:28 +0200

    Design update NetworkManagement

--- Implementation/Admin_dashboard/src/Components/NetworkComponent.tsx
+++ Implementation/Admin_dashboard/src/Components/NetworkComponent.tsx
@@ -1,4 +1,4 @@
-
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 function NetworkComponent() {
     return (
@@ -16,12 +16,12 @@ function NetworkComponent() {
             </div>
 
             <div className="flex flex-col gap-5 lg:flex-row items-center lg:h-full w-full  lg:overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] ">
-                <div className="flex flex-col p-5 rounded-2xl border border-[#E9E2CD] w-full shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] bg-white gap-3 lg:w-1/3 h-[100%]">
+                <div className="flex flex-col p-6 rounded-2xl border border-[#E9E2CD] w-full shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] bg-white gap-3 lg:w-1/3 h-full min-h-0">
                     <h1 className="text-[#658F8D] text-center text-2xl font-bold mb-3 lg:text-3xl">
                         Current Network - John Doe - Layer 1
                     </h1>
 
-                    <div className="flex flex-1 flex-col gap-3 overflow-y-auto pr-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-7">
+                    <div className="flex flex-1 flex-col gap-3 min-h-0 overflow-y-auto pr-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-7 pb-2">
                         {Array.from({ length: 10 }).map((_, index) => (
                             <div className="flex flex-row items-center justify-between text-xl" key={index}>
                                 <h1> John Doe </h1>
@@ -35,13 +35,16 @@ function NetworkComponent() {
 
 
 
-                <div className="flex flex-col p-4 gap-4 w-full lg:flex-row lg:gap-0 lg:w-2/3 h-[100%] lg:bg-white lg:rounded-2xl lg:border lg:border-[#E9E2CD] lg:shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] ">
+                <div className="flex flex-col lg:p-3 gap-4 w-full lg:flex-row lg:gap-0 lg:w-2/3 h-[100%] lg:bg-white lg:rounded-2xl lg:border lg:border-[#E9E2CD] lg:shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] ">
                     <div className="flex flex-col  rounded-2xl border border-[#E9E2CD] p-4 w-full shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] bg-white gap-3 lg:bg-transparent lg:shadow-none lg:border-0 ">
-                        <h1 className="text-[#658F8D] text-2xl text-center font-bold lg:text-3xl"> Available Buddies </h1>
-                        <input placeholder="Search buddies" className="p-2 border rounded-xl border-[#B7C0B2] bg-[#FEF5E4] placeholder:text-[#658F8D] placeholder:font-semibold font-semibold focus:outline-[#B7C0B2] " />
-                        <div className="flex flex-col gap-2 lg:overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                        <h1 className="text-[#658F8D] text-2xl text-center font-bold lg:text-3xl lg:mb-3"> Available Buddies </h1>
+                        <div className='relative w-full'>
+                            <MagnifyingGlassIcon className='w-5 h-5 absolute left-3 top-1/2 -translate-y-1/2 text-[#658F8D]'/>
+                            <input placeholder="Search buddies" className="w-full p-2 pl-10 border rounded-xl border-[#B7C0B2] bg-[#FEF5E4] placeholder:text-[#658F8D] placeholder:font-semibold font-semibold focus:outline-[#B7C0B2] " />
+                        </div>
+                        <div className="flex flex-col min-h-0 gap-2 lg:overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
                             {Array.from({ length: 3 }).map((_, index) => (
-                                <div className="flex flex-col border border-[#B7C0B2] rounded-xl p-3 bg-[#FEF5E4] gap-1" key={index}>
+                                <div className="flex flex-col border border-[#B7C0B2] rounded-xl py-2 px-3 bg-[#FEF5E4] gap-1" key={index}>
                                     <div className="flex flex-row justify-between items-center">
                                         <h1 className="font-semibold text-2xl"> John Doe </h1>
                                         <button className="bg-[#658F8D] text-white rounded-2xl text-md border border-[#5B7C6F] font-bold shadow-[0_4px_4px_0_rgba(0,0,0,0.1)] px-3 py-2 cursor-pointer hover:bg-[#739B99] active:scale-[0.98] transition-all duration-150 ease-in-out">Add</button>
@@ -54,15 +57,15 @@ function NetworkComponent() {
                     </div>
 
                     <div className="flex flex-col p-4 overflow-hidden  rounded-2xl border border-[#E9E2CD] w-full shadow-[0_1px_8px_0_rgba(0,0,0,0.1)] lg:shadow-none lg:border-0 bg-white">
-                        <h1 className="text-[#658F8D] text-2xl font-bold text-center mb-3 lg:text-3xl lg:mb-0"> Pending Network Changes </h1>
+                        <h1 className="text-[#658F8D] text-2xl font-bold text-center mb-3 lg:text-3xl "> Pending Network Changes </h1>
 
-                        <div className="flex flex-col overflow-hidden h-full mt-4 gap-3 lg:gap-0">
-                            <div className="flex flex-col gap-2 min-h-[45%]  ">
+                        <div className="flex flex-col overflow-hidden h-full mt-4 lg:gap-2">
+                            <div className="flex flex-col gap-2 flex-1 min-h-0  ">
                                 <h2 className="text-[#658F8D] text-xl font-bold lg:text-xl"> To Be Added </h2>
 
-                                <div className="flex  flex-col gap-3 overflow-y-auto pr-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-3">
+                                <div className="flex  flex-col min-h-0 gap-3 overflow-y-auto pr-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-3">
                                     {Array.from({ length: 7 }).map((_, index) => (
-                                        <div className="flex flex-row justify-between items-center border-3 border-[#90EE90] bg-[#FEF5E4] rounded-xl px-2 py-2" key={index}>
+                                        <div className="flex flex-row justify-between items-center border-3 border-[#90EE90] bg-[#FEF5E4] rounded-xl px-2 py-1" key={index}>
                                             <h1 className="font-medium"> John Doe </h1>
                                             <button className="bg-[#658F8D] text-white rounded-2xl text-md border border-[#5B7C6F] font-bold shadow-[0_4px_4px_0_rgba(0,0,0,0.1)] px-4 py-1 cursor-pointer hover:bg-[#739B99] active:scale-[0.98] transition-all duration-150 ease-in-out">Undo</button>
                                         </div>
@@ -70,12 +73,12 @@ function NetworkComponent() {
                                 </div>
                             </div>
 
-                            <div className="flex flex-col gap-2  min-h-[45%]">
+                            <div className="flex flex-col gap-2  flex-1 min-h-0">
                                 <h2 className="text-[#658F8D] text-xl font-bold lg:text-xl"> To Be Removed </h2>
 
                                 <div className="flex  flex-col gap-3 overflow-y-auto  [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-3">
                                     {Array.from({ length: 7 }).map((_, index) => (
-                                        <div className="flex flex-row justify-between items-center border-3 border-[#FF7F7F] bg-[#FEF5E4] rounded-xl px-2 py-2 border-t-2" key={index}>
+                                        <div className="flex flex-row justify-between items-center border-3 border-[#FF7F7F] bg-[#FEF5E4] rounded-xl px-2 py-1 border-t-2" key={index}>
                                             <h1 className="font-medium"> John Doe </h1>
                                             <button className="bg-[#658F8D] text-white rounded-2xl text-md border border-[#5B7C6F] font-bold shadow-[0_4px_4px_0_rgba(0,0,0,0.1)] px-4 py-1 cursor-pointer hover:bg-[#739B99] active:scale-[0.98] transition-all duration-150 ease-in-out">Undo</button>
                                         </div>
