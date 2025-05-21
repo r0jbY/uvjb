f9544aa133190e147277ed86438ac40c30ca1fcb
r0jbY <r.balint@student.fontys.nl>
2025-05-21 11:05:09 +0200

    Testing pipeline.

--- Implementation/MicroserviceBackend/AuthenticationService/src/utils/catchAsync.ts
+++ Implementation/MicroserviceBackend/AuthenticationService/src/utils/catchAsync.ts
@@ -4,7 +4,7 @@ const catchAsync = (
   fn: (req: Request, res: Response, next: NextFunction) => Promise<Response>
 ): RequestHandler => {
   return (req, res, next) => {
-    fn(req, res, next).catch(next); 
+    fn(req, res, next).catch(next);  
   };
 };
 
