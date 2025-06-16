22547ed4c25e64e271aa387a629175728a0d65fa
RobertBalint <117170247+r0jbY@users.noreply.github.com>
2025-06-16 12:25:40 +0200

    Delete Implementation/admind_dadsboard directory

--- Implementation/admind_dadsboard/src/pages/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Profile = () => {
-    return <h2>Profile Content</h2>;
-};
-
-export default Profile;
--- Implementation/admind_dadsboard/src/pages/Settings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Settings = () => {
-    return <h2>Settings Content</h2>;
-};
-
-export default Settings;
