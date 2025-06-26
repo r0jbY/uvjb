81fa574cf788ce023ba90d5e773146fa8e5e6cba
r0jbY <r.balint@student.fontys.nl>
2025-06-26 11:46:14 +0200

    Update schemas.ts

--- Implementation/MicroserviceBackend/NotificationService/src/utils/schemas.ts
+++ Implementation/MicroserviceBackend/NotificationService/src/utils/schemas.ts
@@ -4,8 +4,5 @@ export const pushSchema = z.object({
   id: z.string().uuid(),
   token: z
     .string()
-    .regex(/^ExponentPushToken\[[A-Za-z0-9]{10,}\]$/, {
-      message: 'Invalid Expo push token format',
-    }),
 });
 
